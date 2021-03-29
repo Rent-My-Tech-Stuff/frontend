78dafe7d69f7b9731328d8bb2ad24324be85a793
Taylor Friesen <taylorfriesen@protonmail.com>
2021-03-28 19:48:04 -0700

    created redux reducer and store

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
 import {Route} from 'react-router-dom';
 
+import {createStore, applyMiddleware} from 'redux';
+import {Provider} from 'react-redux';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+
+import {initialState, reducer} from './reducer';
+
 import NavBar from './components/NavBar';
 import SplashPage from './components/SplashPage';
 import Login from './components/Login';
@@ -11,18 +18,22 @@ import ItemPage from './components/ItemPage';
 
 import './App.css';
 
+const store = createStore(reducer, initialState, applyMiddleware(thunk, logger));
+
 function App() {
   return (
-    <div className="App">
-      <NavBar/>
-      <Route exact path='/' component={SplashPage}/>
-      <Route path='/login' component={Login}/>
-      <Route path='/register' component={Register}/>
-      <Route path='/profile' component={Profile}/>
-      <Route path='/owner-home' component={OwnerHome}/>
-      <Route path='/renter-home' component={RenterHome}/>
-      <Route path='/item' component={ItemPage}/>
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <NavBar/>
+        <Route exact path='/' component={SplashPage}/>
+        <Route path='/login' component={Login}/>
+        <Route path='/register' component={Register}/>
+        <Route path='/profile' component={Profile}/>
+        <Route path='/owner-home' component={OwnerHome}/>
+        <Route path='/renter-home' component={RenterHome}/>
+        <Route path='/item' component={ItemPage}/>
+      </div>
+    </Provider>
   );
 }
 
