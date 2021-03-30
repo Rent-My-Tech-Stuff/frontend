f6d3eeea8f18ccb431d678c1a3aa0cb082634876
Taylor Friesen <taylorfriesen@protonmail.com>
2021-03-30 11:34:23 -0700

    updated reducer

--- src/reducer/index.js
+++ src/reducer/index.js
@@ -2,6 +2,21 @@ import {
   LOGIN_START,
   LOGIN_SUCCESS,
   LOGIN_FAILURE,
+  REGISTER_START,
+  REGISTER_SUCCESS,
+  REGISTER_FAILURE,
+  OWNER_SELECT_ITEM,
+  OWNER_CHANGE_ITEM_START,
+  OWNER_CHANGE_ITEM_SUCCESS,
+  OWNER_CHANGE_ITEM_FAILURE,
+  OWNER_NEW_ITEM,
+  OWNER_ADD_ITEM_START,
+  OWNER_ADD_ITEM_SUCCESS,
+  OWNER_ADD_ITEM_FAILURE,
+  OWNER_CANCEL,
+  OWNER_DELETE_ITEM_START,
+  OWNER_DELETE_ITEM_SUCCESS,
+  OWNER_DELETE_ITEM_FAILURE,
 } from '../actions';
 
 export const initialState = {
@@ -17,7 +32,10 @@ export const initialState = {
 export const reducer = (state, action) => {
   switch (action.type) {
     case LOGIN_START:
-      return state;
+      return {
+        ...state,
+        message: 'Logging in...'
+      }
     case LOGIN_SUCCESS:
       localStorage.setItem('token', action.payload.token);
       return {
@@ -30,6 +48,21 @@ export const reducer = (state, action) => {
         ...state,
         message: 'Username or password incorrect'
       }
+    case REGISTER_START:
+    case REGISTER_SUCCESS:
+    case REGISTER_FAILURE:
+    case OWNER_SELECT_ITEM:
+    case OWNER_CHANGE_ITEM_START:
+    case OWNER_CHANGE_ITEM_SUCCESS:
+    case OWNER_CHANGE_ITEM_FAILURE:
+    case OWNER_NEW_ITEM:
+    case OWNER_ADD_ITEM_START:
+    case OWNER_ADD_ITEM_SUCCESS:
+    case OWNER_ADD_ITEM_FAILURE:
+    case OWNER_CANCEL:
+    case OWNER_DELETE_ITEM_START:
+    case OWNER_DELETE_ITEM_SUCCESS:
+    case OWNER_DELETE_ITEM_FAILURE:
     default:
       return state;
   }
