80c54d15c9093300960766172111c7e53c6cbaf0
kait hunnicutt <katerenehunnicutt@gmail.com>
2021-03-30 16:05:03 -0600

    move page and add mapping

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,38 +1,12 @@
 import React, { useState } from 'react'
 import ProfileForm from './ProfileForm'
-import {
-  renterSearch
-} from "../actions/index"
 
-
-const initialFormValues = {
-  search: "",
-  location: "",
-}
 
 function Profile() {
-
-  const [formValues, setFormValues] = useState(initialFormValues)
-
-  const formSubmit = (value) => {
-    renterSearch(value);
-  }
-
-  const inputChange = (name, value) => {
-    setFormValues({
-      ...formValues, [name]: value,
-    })
-  }
   return (
-    <div>
-      <ProfileForm 
-      values={formValues}
-      change={inputChange}
-      submit={formSubmit}
-      />
-
-    </div>
-  );
+    <>
+    </>
+  )
 }
 
 export default Profile;
