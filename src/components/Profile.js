1ce1b7a2a78c4f8e84e69f098490322cbbad2e58
kait hunnicutt <katerenehunnicutt@gmail.com>
2021-03-30 15:11:00 -0600

    profile search and dropdown ready for review

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,7 +1,36 @@
+import React, { useState } from 'react'
+import ProfileForm from './ProfileForm'
+import {
+  renterSearch
+} from "../actions/index"
+
+
+const initialFormValues = {
+  search: "",
+  location: "",
+}
+
 function Profile() {
+
+  const [formValues, setFormValues] = useState(initialFormValues)
+
+  const formSubmit = (value) => {
+    renterSearch(value);
+  }
+
+  const inputChange = (name, value) => {
+    setFormValues({
+      ...formValues, [name]: value,
+    })
+  }
   return (
     <div>
-      profile page goes here
+      <ProfileForm 
+      values={formValues}
+      change={inputChange}
+      submit={formSubmit}
+      />
+
     </div>
   );
 }
---
+++ src/components/ProfileForm.js
@@ -0,0 +1,57 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+
+const StyledPage = styled.div``;
+const StyledSearch = styled.div``;
+const StyledDropdown = styled.div``;
+
+const ProfileForm = (props) => {
+  const initialValue = "";
+  const { values, change, submit, disabled } = props;
+  const [input, setInput] = useState(initialValue);
+
+  const onSubmit = (evt) => {
+    evt.preventDefault();
+    submit(input);
+  };
+
+  const onChange = (evt) => {
+    const { value } = evt.target;
+    setInput(value);
+  };
+
+  return (
+    <StyledPage>
+      <form className="profileFormContainer" onSubmit={onSubmit}>
+        <StyledDropdown>
+          <label>
+            Filter Results
+            <select
+              values={values.location}
+              name="location"
+              onChange={onChange}
+            >
+              <option value="">---</option>
+              <option value="zipcode">zip code</option>
+              <option value="city">city</option>
+              <option value="state">state</option>
+            </select>
+          </label>
+        </StyledDropdown>
+        <StyledSearch>
+          <label>
+            Search Bar
+            <input
+              values={input}
+              onChange={onChange}
+              name="search"
+              type="text"
+            />
+          </label>
+          <button disabled={disabled}>Search</button>
+        </StyledSearch>
+      </form>
+    </StyledPage>
+  );
+};
+export default ProfileForm;
