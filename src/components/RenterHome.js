133a0230c4a2994cfa77c3edecf5720dfbddc483
kait hunnicutt <katerenehunnicutt@gmail.com>
2021-03-31 15:20:04 -0600

    routing and click handler added to renter-home

--- src/components/RenterHome.js
+++ src/components/RenterHome.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
 import RenterHomeForm from "./RenterHomeForm";
-
 import { renterSearch } from "../actions/index";
 import { useDispatch } from "react-redux";
 
@@ -8,7 +8,9 @@ const initialFormValues = {
   search: "",
   location: "",
 };
+
 function RenterHome() {
+  const history = useHistory();
   const [formValues, setFormValues] = useState(initialFormValues);
   const dispatch = useDispatch();
   //dummy data same as owner
@@ -19,6 +21,7 @@ function RenterHome() {
       price_per_day: "25",
       rental_period: "7 days",
       description: "good phone",
+      city: "Seattle",
       user_id: 1,
     },
     {
@@ -27,6 +30,7 @@ function RenterHome() {
       price_per_day: "5",
       rental_period: "7 days",
       description: "listen up",
+      city: "Seattle",
       user_id: 2,
     },
     {
@@ -35,12 +39,19 @@ function RenterHome() {
       price_per_day: "30",
       rental_period: "5 days",
       description: "type type type",
+      city: "Seattle",
       user_id: 3,
     },
   ];
 
   const formSubmit = () => {
     dispatch(renterSearch(formValues)); // action
+  };
+
+  const handleDisplayClick = (e) => {
+    e.preventDefault();
+    history.push("/item");
+    console.log('clicked')
   };
 
   const inputChange = (name, value) => {
@@ -56,13 +67,16 @@ function RenterHome() {
         change={inputChange}
         submit={formSubmit}
       />
-      <div className="itemResults">
+      <div className="itemResults" >
         {posts &&
           posts.map((item, i) => (
-            <div className="itemCard" key={i}>
+            <div className="itemCard" key={i} 
+            to="/item"
+            onClick={(e) => handleDisplayClick(e, item)}>
               <h2>{item.name}</h2>
-              <p>{item.price_per_day}</p>
-              <p>{item.description}</p>
+              <p>${item.price_per_day} per day</p>
+              <p>located in {item.city}</p>
+              <p>{item.description}</p> 
             </div>
           ))}
       </div>
