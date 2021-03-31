59e278aa130bf49988189a93e344d7f5c2d7822f
joe-park <parkjs3@yahoo.com>
2021-03-31 13:45:53 -0700

    successful register api call

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,84 +1,101 @@
-import React, {useState, useEffect} from 'react'
-import registrationSchema from './RegistrationSchema'
-import RegistrationForm from "./RegistrationForm"
+import React, { useState, useEffect } from "react";
+import registrationSchema from "./RegistrationSchema";
+import RegistrationForm from "./RegistrationForm";
 import * as yup from "yup";
-import styled from 'styled-components'
+import styled from "styled-components";
+
+import { connect } from "react-redux";
+import { useHistory } from "react-router-dom";
+import { register } from "../actions";
 
 const initialFormValues = {
-    username : "",
-    password : "",
-    email : "",
-    firstname : "",
-    lastname : "",
-    streetAddress : "",
-    city : "",
-    state : "",
-    zipcode : "",
-    //radio button
-    role : "",
-}
+  username: "",
+  password: "",
+  email: "",
+  firstname: "",
+  lastname: "",
+  streetAddress: "",
+  city: "",
+  state: "",
+  zipcode: "",
+  //radio button
+  role: "",
+};
 
 const initialFormErrors = {
-    username : "",
-    password : "",
-    email : "",
-    firstname : "",
-    lastname : "",
-    streetAddress : "",
-    city : "",
-    state : "",
-    zipcode : "",
-    //dropdown
-    role : "",
-}
+  username: "",
+  password: "",
+  email: "",
+  firstname: "",
+  lastname: "",
+  streetAddress: "",
+  city: "",
+  state: "",
+  zipcode: "",
+  //dropdown
+  role: "",
+};
 
-const initialDisabled = true
-
+const initialDisabled = true;
 
 const Register = (props) => {
+  const { user, register } = props;
+  const history = useHistory();
 
-const [formValues, setFormValues] = useState(initialFormValues);
-const [formErrors, setFormErrors] = useState(initialFormErrors);
-const [disabled, setDisabled] = useState(initialDisabled);
+  const [formValues, setFormValues] = useState(initialFormValues);
+  const [formErrors, setFormErrors] = useState(initialFormErrors);
+  const [disabled, setDisabled] = useState(initialDisabled);
 
-const inputChange = (name, value) => {
-  yup
-  .reach(registrationSchema, name)
-  .validate(value)
-  .then(() => {
-    setFormErrors({...formErrors, [name]: "",})
-  })
-  .catch((err) => {
-    setFormErrors({...formErrors, [name]: err.errors[0]})
-  })
-  setFormValues({
-    ...formValues, [name]: value
-  })
-}
+  useEffect(() => {
+    if (user) {
+      if (user.role === "owner") {
+        history.push("/login");
+      } else if (user.role === "renter") {
+        history.push("/login");
+      }
+    }
+  }, [user, history]);
 
-const formSubmit = () => {
-  const loginSubmit = {
-    username: formValues.username.trim(),
-    password: formValues.password.trim(),
-    email: formValues.email.trim(),
-    firstName: formValues.firstName.trim(),
-    lastName: formValues.lastName.trim(),
-    streetAddress: formValues.streetAddress.trim(),
-    city: formValues.city.trim(),
-    state: formValues.state.trim(),
-    zipcode: formValues.zipcode.trim(),
-    role: formValues.role.trim(),
-  }
-}
+  const inputChange = (name, value) => {
+    yup
+      .reach(registrationSchema, name)
+      .validate(value)
+      .then(() => {
+        setFormErrors({ ...formErrors, [name]: "" });
+      })
+      .catch((err) => {
+        setFormErrors({ ...formErrors, [name]: err.errors[0] });
+      });
+    setFormValues({
+      ...formValues,
+      [name]: value,
+    });
+  };
 
-useEffect(() => {
-  registrationSchema.isValid(formValues).then((valid) => setDisabled(!valid));
-}, [formValues]
-)
+  const formSubmit = () => {
+    const loginSubmit = {
+      username: formValues.username.trim(),
+      password: formValues.password.trim(),
+      email: formValues.email.trim(),
+      firstname: formValues.firstname.trim(),
+      lastname: formValues.lastname.trim(),
+      streetAddress: formValues.streetAddress.trim(),
+      city: formValues.city.trim(),
+      state: formValues.state.trim(),
+      zipcode: formValues.zipcode.trim(),
+      role: formValues.role.trim(),
+    };
+    console.log(formValues);
+    register(formValues);
+  };
+
+  useEffect(() => {
+    registrationSchema.isValid(formValues).then((valid) => setDisabled(!valid));
+  }, [formValues]);
 
   return (
     <div>
-      <RegistrationForm 
+      <RegistrationForm
         values={formValues}
         change={inputChange}
         submit={formSubmit}
@@ -87,6 +104,10 @@ useEffect(() => {
       />
     </div>
   );
-}
+};
 
-export default Register;
+const mapStateToProps = (state) => {
+  return { user: state.user };
+};
+
+export default connect(mapStateToProps, { register })(Register);
