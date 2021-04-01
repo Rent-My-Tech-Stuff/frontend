86f6fa9a4c4ff0e137e8c0517eb494a14157c5ba
Taylor Friesen <taylorfriesen@protonmail.com>
2021-03-31 20:21:30 -0700

    cleaned up code on ItemPage

--- src/components/ItemPage.js
+++ src/components/ItemPage.js
@@ -1,48 +1,6 @@
-import { useParams } from 'react-router-dom';
 import {connect} from 'react-redux';
 
-const posts = [
-  {    
-    name: 'iphone',
-    category: 'phone',
-    price_per_day: '25',
-    rental_period: '7 days',
-    description: 'good phone',
-    user_id: 1,
-    id: 1
-  },
-  {
-    name: 'airpods',
-    category: 'headphones',
-    price_per_day: '5',
-    rental_period: '7 days',
-    description: 'listen up',
-    user_id: 2,
-    id: 2
-  },
-  {
-    name: 'macbook', 
-    category: 'pc', 
-    price_per_day: '30', 
-    rental_period: '5 days', 
-    description: 'type type type', 
-    user_id: 3,
-    id: 3
-
-  },
-]
-
 function ItemPage(props) {
-
-  
-  // changed address within App so that it takes specific item id
-  let { id } = useParams();
-
-  // let {thisItem} = props;
-  // console.log(thisItem);
-
-  //may or may not need this function but i needed it to try to use dummy data
-  // const product = posts.find(p => p.id === parseInt(id))
   const product = props.thisItem;
 
   return product ? (
@@ -52,9 +10,6 @@ function ItemPage(props) {
       <p>{product.price_per_day}</p>
       <p>{product.rental_period}</p>
       <p>{product.description}</p>
-      
-      {/* not sure how the data coming in is organized so labeled this username/email hoping theyre in same index as the post information */}
-
       <p>{product.firstname} {product.lastname}</p>
       <p>{product.email}</p>
     </div>
